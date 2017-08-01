015678f8a522e8735d52f475d856ffce6a9a94a6
chichone <cakyle@gmail.com>
2017-07-31 17:11:04 -0700

    Print results from first two queries

--- newsapp.py
+++ newsapp.py
@@ -1,7 +1,6 @@
 #!python3
 import psycopg2
 # import os
-
 #  VVVVVVV  writes/overwites file
 # fn = 'outfile.txt'
 
@@ -18,8 +17,33 @@ import psycopg2
 
 conn = psycopg2.connect("dbname=news")
 c = conn.cursor()
-c.execute("select * from authors;")
+command = 'select articles.title as article, subtable.count as views from articles join (select substring(path, 10) as slug_log, count(*) from log group by path order by count desc limit 3 offset 1) as subtable on articles.slug = subtable.slug_log order by count desc;'
+c.execute(command)
 rows = c.fetchall()
-conn.close()
+print('Three most popular articles:')
+for row in rows:
+    out = '"'
+    out += row[0]
+    out += '"'
+    out += ' \u2014 '
+    out += str(row[1])
+    out += ' views'
+    print(out)
 
-print(rows)
+command = 'select a.name as author, s.sum as "total views" from authors as a join (select sub.author, sum(sub.views) from (select articles.author as author, subtable.count as views from articles join (select substring(path, 10) as slug_log, count(*) from log group by path order by count desc offset 1) as subtable on articles.slug = subtable.slug_log order by count desc) as sub group by sub.author) as s on a.id = s.author order by sum desc;'
+c.execute(command)
+rows = c.fetchall()
+print('\n')
+print('Most popular authors:')
+for row in rows:
+    print('"' + row[0] + ' \u2014 ' + str(row[1]) + ' views')
+print('\n')
+
+# command = """
+# select * from (select ecodes.month, ecodes.day, ecodes.year, round(ecodes.errors*100.0/allent.count, 1) as errorpct from (select extract (month from time) "month", extract(day from time) "day", extract(year from time) "year", count(*) as errors from log where status like '4%' group by (month, day, year) order by errors desc) as ecodes left join (select extract (month from time) "month", extract(day from time) "day", extract(year from time) "year", count(*) from log group by (month, day, year)) as allent on (ecodes.month = allent.month) and (ecodes.day = allent.day) and (ecodes.year = allent.year)) as q where q.errorpct > 1;
+# """
+# c.execute(command)
+# rows = c.fetchall()
+# print(rows)
+
+conn.close()
